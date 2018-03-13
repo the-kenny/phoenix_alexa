6f8111fce1649a4d266229d4ddc9f1b87eba09ca
Moritz Ulrich <moritz@tarn-vedra.de>
2018-03-13 23:55:03 +0100

    Update dependencies and fix warnings.

--- mix.exs
+++ mix.exs
@@ -8,8 +8,8 @@ defmodule PhoenixAlexa.Mixfile do
      version: @version,
      elixir: "~> 1.2",
      description: "Alexa library for Phoenix",
-     deps: deps,
-     package: package,
+     deps: deps(),
+     package: package(),
      consolidate_protocols: Mix.env != :test]
   end
 
@@ -18,8 +18,8 @@ defmodule PhoenixAlexa.Mixfile do
   end
 
   defp deps do
-    [{:poison, "~> 2.0"},
-     {:plug, "~> 1.1"},
+    [{:poison, "~> 3.1"},
+     {:plug, "~> 1.5"},
     ]
   end
 
